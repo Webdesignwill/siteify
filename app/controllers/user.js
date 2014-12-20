aa5539acadd5a587136a291140b23167b9545a0f
Webdesignwill <will@webdesignwill.co.uk>
2014-12-20 18:42:46 +0100

    Debug

--- app/controllers/user.js
+++ app/controllers/user.js
@@ -49,9 +49,7 @@ module.exports.unique = function (req, res, next) {
 /* Get me as the user
 ============================= */
 module.exports.me = function (req, res, next) {
-  console.log('REQ USER ************************** : ', req.user);
   User.findById(req.user.id, function (err, user) {
-    console.log('USER ************************** : ', user);
     if (err) next(err);
     res.json(parseUserObject(user));
   });
